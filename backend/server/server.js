07c25f12ea656eb15286d6279de984a67035a598
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-06-05 19:26:38 -0400

    moved server stuff into backend folder and added database.js. server and database are connected

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const SpotifyWebApi = require('spotify-web-api-node');
-
-
-
-
-
 
 const app = express();
+
+const database = require('../database/database');
+database.initialize(); //database first so that you can make sure it is live before you run server.
+
+
+
 
 
 const port = 3000;
