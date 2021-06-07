a133f7f8f8efbaa6f979c6f152f46382c4b89a40
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-06-07 16:05:10 -0400

    moved server and set it up to work with routes and database

---
+++ back-end/server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const database = require('../database/database');
+database.initialize(); //database first so that you can make sure it is live before you run server.
+
+
+
+
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'))
+    .use(cors())
+
+
+
+
+const userStats = require('../routes/user_stats');
+app.use('/', userStats);
+
+app.listen(port, () =>
+    console.log(`server is running on ${port}`));
