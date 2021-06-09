414cc927bf5b64115b2433901529c4453eff7dd2
Ami Ross <amiross82792@gmail.com>
2021-06-08 22:35:59 -0400

    Created Analytics Interface to work with our backend calls

---
+++ ReMuseMe/src/app/Interfaces/analytics.ts
@@ -0,0 +1,8 @@
+export interface Analytics {
+    song_id: string,
+    danceability: number,
+    energy: number, 
+    acousticness: number,
+    instrumentalness: number,
+    valence: number,
+}
