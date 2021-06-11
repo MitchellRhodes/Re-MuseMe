b849deec14d513f96892d678418fbce6593a31c9
Mitchell Rhodes <M.Rhodes87@comcast.net>
2021-06-10 23:33:49 -0400

    added swipe interface, changed get songId call to get songIDs that haven't been swiped on by user.

--- ReMuseMe/src/app/database.service.ts
+++ ReMuseMe/src/app/database.service.ts
@@ -1,12 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Swipe } from './Interfaces/swipe';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+
+  createJson = {
+    headers: new HttpHeaders({ 'Content-type': 'application/json' })
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -15,4 +20,9 @@ export class DatabaseService {
 
     return this.http.get(`http://localhost:3000/song-data`) as Observable<any>;
   };
+
+  async postSwipe(swipe: Swipe) {
+    return this.http.post(`http://localhost:3000/swipes`, swipe, this.createJson).subscribe(res => console.log(res))
+  }
+
 }
