865903804d8f11859524874d2b4897cdd39ef608
Ami Ross <amiross82792@gmail.com>
2021-06-19 14:44:42 -0400

    updating api calls to secure

--- ReMuseMe/src/app/database.service.ts
+++ ReMuseMe/src/app/database.service.ts
@@ -16,36 +16,36 @@ export class DatabaseService {
   constructor(private http: HttpClient) { }
 
   async getAllSongsNotSwiped(id: number) {
-    return this.http.get(`http://api.swipify.me/user/${id}/song-data`);
+    return this.http.get(`https://api.swipify.me/user/${id}/song-data`);
   }
 
   async postSwipe(swipe: Swipe) {
-    return this.http.post(`http://api.swipify.me/swipes`, swipe, this.createJson).subscribe(res => console.log(res))
+    return this.http.post(`https://api.swipify.me/swipes`, swipe, this.createJson).subscribe(res => console.log(res))
   }
 
   async putSwipe(swipe: Swipe, userId: number, id: number) {
-    return this.http.put(`http://api.swipify.me/user/${userId}/swipes/${id}`, swipe, this.createJson).subscribe(res => console.log(`put swipe`, res))
+    return this.http.put(`https://api.swipify.me/user/${userId}/swipes/${id}`, swipe, this.createJson).subscribe(res => console.log(`put swipe`, res))
   }
 
   async getUsers() {
-    return this.http.get(`http://api.swipify.me/user`);
+    return this.http.get(`https://api.swipify.me/user`);
   }
 
   async getUser(email: string) {
 
-    return this.http.get(`http://api.swipify.me/user/${email}`);
+    return this.http.get(`https://api.swipify.me/user/${email}`);
   }
 
   async changeSongStringIdToNumber(id: string) {
-    return this.http.get(`http://api.swipify.me/song/${id}`);
+    return this.http.get(`https://api.swipify.me/song/${id}`);
   }
 
   async postUser(user: User) {
-    return this.http.post(`http://api.swipify.me/user`, user, this.createJson).subscribe(res => console.log(`service`, res))
+    return this.http.post(`https://api.swipify.me/user`, user, this.createJson).subscribe(res => console.log(`service`, res))
   }
 
   async getUserStats(id: number) {
-    return this.http.get(`http://api.swipify.me/user-stats/${id}`);
+    return this.http.get(`https://api.swipify.me/user-stats/${id}`);
   }
 
 }
